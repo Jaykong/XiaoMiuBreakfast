ad692d09f68cc9b0ce9d0019a0102d38c80bf221
kongyunpeng2011@sina.com <kongyunpeng2011@sina.com>
2018-09-17 22:34:31 +0800

    clean up

--- server/controllers/demo.js
+++ server/controllers/demo.js
@@ -1,6 +1,7 @@
 const { mysql } = require('../qcloud')
 module.exports = async ctx => {
- 
+  var inserted = await mysql('orderInfo').insert({ id: 5, foods: '{"food1":3,"food2":4}' })
+
   var json = await mysql("orderInfo").select('*')
   ctx.state.data = {
     msg: json
